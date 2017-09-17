67fce750c6a1352f27929b9c805449ed40704ed7
areednehvi <areednazir@gmail.com>
2017-09-17 07:31:26 -0700

    Assembly Change
    Assembly Change

--- School Universe/School Universe/Properties/AssemblyInfo.cs
+++ School Universe/School Universe/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("School Universe")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Windows app for School Universe")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Arif Nazir")]
 [assembly: AssemblyProduct("School Universe")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -53,3 +53,5 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-IN")]
+
