dee761550c1f3abe9b311f1f0739347bf4499f5d
areednehvi <areednazir@gmail.com>
2017-09-21 09:44:02 -0700

    Grid
    grid

---
+++ School Universe/School Universe/Views/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Universe.Views
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public uint Age { get; set; }
+
+    }
+}
