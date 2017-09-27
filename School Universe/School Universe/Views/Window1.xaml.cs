b5342379c9e967754f22451913e2b76701820e76
Arif Nazir <arif.nazir@bqe.com>
2017-09-27 15:08:46 +0530

    Menu example
    Menu example

---
+++ School Universe/School Universe/Views/Window1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace School_Universe.Views
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (rect.Visibility == System.Windows.Visibility.Collapsed)
+            {
+                rect.Visibility = System.Windows.Visibility.Visible;
+                (sender as Button).Content = "<";
+            }
+            else
+            {
+                rect.Visibility = System.Windows.Visibility.Collapsed;
+                (sender as Button).Content = ">";
+            }
+        }
+    }
+}
