0e172476ef28fc46e9a5e54f496b44ab50042d8c
Arif Nazir <arif.nazir@bqe.com>
2017-11-07 13:00:37 +0530

    Added sync stored procedures
    Added sync stored procedures

--- School Universe/School Universe Businness Layer/Shared/StoredProcedures.cs
+++ School Universe/School Universe Businness Layer/Shared/StoredProcedures.cs
@@ -23,6 +23,26 @@ namespace School_Universe_Businness_Layer.Shared
         public static string GetSettings = "GetSettings";
         public static string GetSyncTableInfo = "GetSyncTableInfo";
         public static string UpdateSyncTableInfo = "UpdateSyncTableInfo";
+        public static string SyncFee_categories = "SyncFee_categories";
+        public static string SyncFees = "SyncFees";
+        public static string SyncFiles = "SyncFiles";
+        public static string SyncGrade_fees = "SyncGrade_fees";
+        public static string SyncGrades = "SyncGrades";
+        public static string SyncParents = "SyncParents";
+        public static string SyncRoute_stops = "SyncRoute_stops";
+        public static string SyncRoute_vehicle_stops = "SyncRoute_vehicle_stops";
+        public static string SyncRoute_vehicle_stops_fee_logs = "SyncRoute_vehicle_stops_fee_logs";
+        public static string SyncRoute_vehicles = "SyncRoute_vehicles";
+        public static string SyncRoutes = "SyncRoutes";
+        public static string SyncSections = "SyncSections";
+        public static string SyncSessions = "SyncSessions";
+        public static string SyncStudent_fees = "SyncStudent_fees";
+        public static string SyncStudent_grade_session_log = "SyncStudent_grade_session_log";
         public static string SyncStudent_payments = "SyncStudent_payments";
+        public static string SyncStudents = "SyncStudents";
+        public static string SyncTrip_stops = "SyncTrip_stops";
+        public static string SyncUser_avatar_files = "SyncUser_avatar_files";
+        public static string SyncUsers = "SyncUsers";
+        public static string SyncVehicles = "SyncVehicles";
     }
 }
